df0636383325a1c1b9d688fa0212c7dc70fc639b
Majid Joseph <majid.joseph7@gmail.com>
2020-12-14 20:39:56 -0500

    set up auth middleware

---
+++ API/middleware/auth-middleware.js
@@ -0,0 +1,4 @@
+exports.isAuthenticated = (req, res, next) => {
+    if(req.user) next();
+    else return res.status(401).json('Unauthorized');
+}
--- API/server.js
+++ API/server.js
@@ -6,6 +6,8 @@ require('./utilities/mongo-config');
 
 const cors = require('cors');
 
+const cookieSession = require('cookie-session');
+
 const passport = require('passport');
 const passportConfig = require('./utilities/passport-config');
 passportConfig.config(passport);
@@ -13,6 +15,10 @@ passportConfig.config(passport);
 const routes = {
     auth: require('./routes/auth-route'),
 }
+
+app.use(cookieSession({
+    keys: [process.env.COOKIE_KEY]
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -22,16 +28,6 @@ app.use(cors({
 
 // Routes
 app.use('/auth', routes.auth);
-
-// TEMP
-app.get('/', (req, res) => {
-    res.send('yo');
-})
-
-// Temp
-app.get('/bad', (req, res) => {
-    res.send('Auth Failure');
-})
 
 app.listen(process.env.BACKEND_PORT, () => {
     console.log(`Listening on port ${process.env.BACKEND_PORT}`);
